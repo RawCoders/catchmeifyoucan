482dc3f27d71b0ed885b732518201dc52701c873
Suraj Shetty <surajshetty3416@gmail.com>
2020-09-13 09:26:09 +0530

    feat: Add MoveAhead Story

--- src/stories.js
+++ src/stories.js
@@ -24,5 +24,18 @@ export default {
 			steps: 200
 		}],
 		onBoundaryCollision: () => {}
+	},
+	"moveAhead" : {
+		movements: [{
+			action: 'moveRight',
+			steps: 100
+		}, {
+			action: 'stop',
+			steps: 200
+		}, {
+			action: 'moveRight',
+			steps: 200
+		}],
+		onBoundaryCollision: () => {}
 	}
 }
